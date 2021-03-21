2973c387107097eab2db74d35739106012231ad6
Jing <wandertosky@hotmail.com>
2021-03-21 13:48:07 -0400

    Modify db schema

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS config_json;
 
 CREATE TABLE config_json(
-    app_code VARCHAR(250) NOT NULL COMMENT 'application code',
-    version VARCHAR(250) NOT NULL COMMENT 'version of config',
-    hash_code VARCHAR(250) NOT NULL COMMENT 'hash of config content',
-    json_conf VARCHAR(250) NOT NULL COMMENT 'json of config content',
+    app_code VARCHAR(45) NOT NULL COMMENT 'application code',
+    version VARCHAR(15) NOT NULL COMMENT 'version of config',
+    hash_code VARCHAR(32) NOT NULL COMMENT 'hash of config content',
+    json_conf VARCHAR(255) NOT NULL COMMENT 'json of config content',
     creation_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'added or updated time',
     PRIMARY KEY(app_code, version)
 );
